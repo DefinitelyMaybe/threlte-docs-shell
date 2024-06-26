92e5eb5c4573525669976283aa12a85fc70365b1
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2024-06-27 11:28:26 +1200

    random content for sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,52 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightVersions from "starlight-versions";
+import starlightLinksValidator from "starlight-links-validator";
+import starlightUtils from "@lorenzo_lewis/starlight-utils";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Threlte",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Learn",
+          autogenerate: { directory: "learn" },
+        },
+        {
+          label: "Examples",
+          autogenerate: { directory: "examples" },
+        },
+        { label: "Core", autogenerate: { directory: "reference/core" } },
+        {
+          label: "Extras",
+          autogenerate: { directory: "reference/extras" },
+        },
+      ],
+      locales: {
+        root: {
+          lang: "en",
+          label: "English",
+        },
+        "zh-cn": {
+          label: "简体中文",
+          lang: "zh-CN",
+        },
+      },
+      editLink: {
+        baseUrl: "https://github.com/threlte/threlte/edit/main/apps/docs",
+      },
+      plugins: [
+        starlightUtils({ multiSidebar: { switcherStyle: "dropdown" } }),
+        starlightLinksValidator(),
+        // starlightVersions({
+        //   versions: [{ slug: "1.0" }],
+        // }),
+      ],
+    }),
+  ],
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,9 @@
-import { defineCollection } from 'astro:content';
-import { docsSchema } from '@astrojs/starlight/schema';
+import { defineCollection } from "astro:content";
+import { docsSchema, i18nSchema } from "@astrojs/starlight/schema";
+import { docsVersionsSchema } from "starlight-versions/schema";
 
 export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
+  docs: defineCollection({ schema: docsSchema() }),
+  i18n: defineCollection({ type: "data", schema: i18nSchema() }),
+  versions: defineCollection({ type: "data", schema: docsVersionsSchema() }),
 };
