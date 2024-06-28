65a9a59a4730c32950df36cb1bc9f0e7a2df2f22
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2024-06-28 15:44:42 +1200

    remove versions. add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,51 +3,56 @@ import starlight from "@astrojs/starlight";
 import starlightLinksValidator from "starlight-links-validator";
 // import starlightVersions from "starlight-versions";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: "Threlte",
-      social: {
-        github: "https://github.com/withastro/starlight",
+  integrations: [starlight({
+    title: "Threlte",
+    social: {
+      github: "https://github.com/withastro/starlight"
+    },
+    sidebar: [{
+      label: "Learn",
+      autogenerate: {
+        directory: "learn"
+      }
+    }, {
+      label: "Examples",
+      autogenerate: {
+        directory: "examples"
+      }
+    }, {
+      label: "Core",
+      autogenerate: {
+        directory: "reference/core"
+      }
+    }, {
+      label: "Extras",
+      autogenerate: {
+        directory: "reference/extras"
+      }
+    }],
+    locales: {
+      root: {
+        lang: "en",
+        label: "English"
       },
-      sidebar: [
-        {
-          label: "Learn",
-          autogenerate: { directory: "learn" },
-        },
-        {
-          label: "Examples",
-          autogenerate: { directory: "examples" },
-        },
-        { label: "Core", autogenerate: { directory: "reference/core" } },
-        {
-          label: "Extras",
-          autogenerate: { directory: "reference/extras" },
-        },
-      ],
-      locales: {
-        root: {
-          lang: "en",
-          label: "English",
-        },
-        "zh-cn": {
-          label: "简体中文",
-          lang: "zh-CN",
-        },
-      },
-      editLink: {
-        baseUrl: "https://github.com/threlte/threlte/edit/main/apps/docs",
-      },
-      components: {
-        Sidebar: "./src/components/sidebar/Sidebar.astro",
-      },
-      plugins: [
-        // starlightVersions({
-        //   versions: [{ slug: "1.0" }],
-        // }),
-        starlightLinksValidator(),
-      ],
-    }),
-  ],
+      "zh-cn": {
+        label: "简体中文",
+        lang: "zh-CN"
+      }
+    },
+    editLink: {
+      baseUrl: "https://github.com/threlte/threlte/edit/main/apps/docs"
+    },
+    components: {
+      Sidebar: "./src/components/sidebar/Sidebar.astro"
+    },
+    plugins: [
+    // starlightVersions({
+    //   versions: [{ slug: "1.0" }],
+    // }),
+    starlightLinksValidator()]
+  }), svelte()]
 });
