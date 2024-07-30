3f41bfa7b0eeb126097ad49b238048734fac70fd
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2024-07-30 18:09:41 +1200

    rename and add onmount

---
+++ src/components/packages.ts
@@ -0,0 +1,4 @@
+export const packages = [
+	{ label: 'Learn', href: '/learn/getting-started/intro', path: '/learn' },
+	{ label: 'Extras', href: '/reference/extras/example', path: '/reference/extras' }
+]
