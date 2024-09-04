cea7ea08338e5e26c279ffd7369e7d0adb1cb03d
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2024-09-05 11:56:30 +1200

    starlight update. Working Example component. Added Examples Layout.

---
+++ src/components/Example/SvelteWrapper.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import OpenInStackblitz from './OpenInStackblitz.svelte'
+
+	export let path: string
+	export let files: Record<string, string>
+	export let hideCode: boolean
+	export let hideStackblitz: boolean = false
+
+	let _class = ''
+	export { _class as class }
+</script>
+
+<div
+	class="relative h-[80vh] w-full overflow-hidden rounded-t-md border border-white/20 bg-blue-900 {hideCode &&
+		'!rounded-md'} {_class}"
+>
+	<iframe src="/examples/{path}" title={path}></iframe>
+
+	{#if !hideStackblitz}
+		<div class="absolute bottom-4 right-4">
+			<OpenInStackblitz {files} />
+		</div>
+	{/if}
+</div>
+
+<style>
+	iframe {
+		position: absolute;
+		left: 0;
+		top: 0;
+		height: 100%;
+		width: 100%;
+		border-style: none;
+	}
+</style>
