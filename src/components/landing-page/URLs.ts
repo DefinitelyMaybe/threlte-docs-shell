690f42df0d5dc535a42f8940dac937a08bd5295a
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2024-09-13 10:39:03 +1200

    add services page. button css goes into main css file. URLs go into separate file

---
+++ src/components/landing-page/URLs.ts
@@ -0,0 +1,13 @@
+export const URLs = {
+	docs: {
+		start: '/learn/getting-started/intro',
+		first: '/learn/getting-started/your-first-scene',
+		install: '/learn/getting-started/installation',
+		extras: '/reference/extras/getting-started',
+		plugins: '/learn/advanced/plugins'
+	},
+	services: '/services',
+	github: 'https://github.com/threlte/threlte',
+	twitter: 'https://twitter.com/threlte',
+	discord: 'https://discord.gg/EqUBCfCaGm'
+}
